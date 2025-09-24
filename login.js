58da08e19931563c3b774cbd364bdce5961ba3a4
Hetosis-coder <>
2025-09-24 13:54:22 +0800

    added the login.js

--- login.js
+++ login.js
@@ -1,5 +1,16 @@
 function login(){
-    let email = document.getElementById("email").ariaValueMax;
-
+    //Email: caburaocrizhamay40@gmail.com // error Message: your email is incorrect
+    //Password: Admin_123 // Error Message: your password is incorrect
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("email").value;
     console.log("you eneter:" + " " + email);
+
+    console.log = ("you enter:" + " " + password);
+    console.log = ("you enter:" + " " + password);
+
+    if (email=="caburaocrizhamay40@gmail.com"){
+        console.log("tama ang iyong email");
+    }else 
+        console.log("mali ang iyong email. Pakipaliktan ");
+
 }
