0cf243130727d1919fac928ac044024b5103cadf
ygarjohannamayD. <ygarjohannamay@gmail.com>
2025-10-01 12:34:26 +0800

    change my login.js

--- login.js
+++ login.js
@@ -1,16 +1,36 @@
-function login(){
+function login() {
     //Email: caburaocrizhamay40@gmail.com // error Message: your email is incorrect
     //Password: Admin_123 // Error Message: your password is incorrect
     let email = document.getElementById("email").value;
-    let pass = document.getElementById("email").value;
-    console.log("you eneter:" + " " + email);
+    let password = document.getElementById("email").value;
 
-    console.log = ("you enter:" + " " + password);
+    console.log = ("you enter:" + " " + email);
     console.log = ("you enter:" + " " + password);
 
-    if (email=="caburaocrizhamay40@gmail.com"){
-        console.log("tama ang iyong email");
-    }else 
-        console.log("mali ang iyong email. Pakipaliktan ");
+    if (email == "caburaocrizhamay40@gmail.com") {
+        console.log("Your email is correct:" + email);
+    } else
+        console.log("Your email is incorrect"); {
+
+
+
+        if (password == "Admin_123") {
+            console.log("Your password is correct");
+        } else
+            console.log("Your password is incorrect");
+
+
+
+        if (password == "Admmin_123");
+        console.log("Your password is correct");
+        }
+    console.log("Your password is correct");
+    if (email + password === "caburaocrizhamay40@gmail.com Admin_123") {
+    } else
+        console.log(" Your email and password is correct")
+    console.log("Your email and password ic incorret");
+
+
 
 }
+
