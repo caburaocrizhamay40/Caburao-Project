ec8afeda1deec72067ffc8aeceeb5ed10a179c78
ygarjohannamayD. <ygarjohannamay@gmail.com>
2025-10-01 12:38:41 +0800

    UPDATE MY LOGIN.JS

--- login.js
+++ login.js
@@ -34,3 +34,12 @@ function login() {
 
 }
 
+
+
+//1. If email and password is both correct - display a message "Your email and password is correct!"
+
+//2. If email is correct but password is incorrect - display a message "Your password is incorrect"
+
+//3. If email is incorrect and password is correct - display a message "Your email is incorrect"
+
+//4. and if both email and password is incorrect - display a message "Your email and password is incorrect"
