fb2186267d13f0ef2671dce34a6b2cbe2e0bdcc0
ygarjohannamayD. <ygarjohannamay@gmail.com>
2025-10-01 12:54:46 +0800

    change

--- login.js
+++ login.js
@@ -1,20 +1,20 @@
 function login() {
-    //Email: caburaocrizhamay40@gmail.com // error Message: your email is incorrect
-    //Password: Admin_123 // Error Message: your password is incorrect
+    //email: caburaocrizhamay40@gmail.com // error Message: your email is incorrect
+    //password: Admin_123 // Error Message: your password is incorrect
     let email = document.getElementById("email").value;
-    let password = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
     console.log = ("you enter:" + " " + email);
     console.log = ("you enter:" + " " + password);
 
-    if (email == "caburaocrizhamay40@gmail.com") {
-        console.log("Your email is correct:" + email);
+    if (email === "caburaocrizhamay40@gmail.com"){
+        console.log("Your email is correct");
     } else
         console.log("Your email is incorrect"); {
 
 
 
-        if (password == "Admin_123") {
+        if (password == "Admin_123"){
             console.log("Your password is correct");
         } else
             console.log("Your password is incorrect");
@@ -27,8 +27,8 @@ function login() {
     console.log("Your password is correct");
     if (email + password === "caburaocrizhamay40@gmail.com Admin_123") {
     } else
-        console.log(" Your email and password is correct")
-    console.log("Your email and password ic incorret");
+        console.log (" Your email and password is correct")
+    console.log ("Your email and password ic incorret");
 
 
 
